67ba6962dcaf3c951fbf2ff5306e7caee40da7e7
Marciliot <targino.m@escolar.ifrn.edu.br>
2024-08-09 16:21:52 -0300

    sqlite em arquivo como db

--- server/index.js
+++ server/index.js
@@ -10,21 +10,50 @@ app.listen(3002, () => {
     console.log('server ta rodando na porta 3002');
 });
 
-const db = mysql.createConnection({
-    user: 'root',
-    password: '',
-    host: 'localhost',
-    database: 'bemtevista'
-});
+// MySQL
+//
+// const db = mysql.createConnection({
+//     user: 'root',
+//     password: '',
+//     host: 'localhost',
+//     database: 'bemtevista'
+// });
 
-// Verificando a conexão com o banco de dados
-db.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
+// // Verificando a conexão com o banco de dados
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Erro ao conectar ao banco de dados:', err);
+//         return;
+//     }
+//     console.log('Conectado ao banco de dados!');
+// });
+
+// SQlite
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('./database.sqlite', (error) => {
+    if (error) {
+        console.log(error);
         return;
     }
-    console.log('Conectado ao banco de dados!');
+    db.run('create table if not exists usuarios (id integer primary key, Nome text, Username text, Email text, Senha text)', (error) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        db.run('insert into usuarios (Nome, Username, Email, Senha) values (?, ?, ?, ?)', ['Leonardo Minora', 'minora', 'l.m@ifrn', '123'], (error) => {
+            if (error) {
+                console.log(error);
+                return;
+            }
+            db.all('select * from usuarios', (error, rows) => {
+                console.log(error, rows);
+            });
+        });
+    });
 });
+
+
 
 app.post('/Cadastro', (req, res) => {
     const sentEmail = req.body.Email;
@@ -55,15 +84,29 @@ app.post('/Login', (req, res) => {
     const SQL = 'SELECT * FROM usuarios WHERE Username = ? AND Senha = ?'; // Correção aqui
     const Values = [sentLoginUserName, sentLoginSenha];
 
-    db.query(SQL, Values, (err, results) => {
-        if (err) {
-           res.send({error: err})
+    // MySql
+    // db.query(SQL, Values, (err, results) => {
+    //     if (err) {
+    //        res.send({error: err})
+    //     }
+    //     if (results.length > 0) { // Correção aqui
+    //         res.send(results)
+    //     } 
+    //     else {
+    //         res.send({ message: 'Erro nas credenciais' }) // Resposta apropriada
+    //     }
+    // });
+    db.all(SQL, Values, (error, rows) => {
+        console.log(error, rows);
+        if (error) {
+            res.send({ error: error })
         }
-        if (results.length > 0) { // Correção aqui
-            res.send(results)
-        } 
+        if (rows.length > 0) { // Correção aqui
+            res.send(rows[0])
+        }
         else {
             res.send({ message: 'Erro nas credenciais' }) // Resposta apropriada
         }
+
     });
 });
